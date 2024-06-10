84f800ef0108ecb7d5b57374e6ee7a8e682859f0
Consy <consy@itmine.ru>
2024-06-10 10:45:07 +0300

    Hello World!

--- src/sus2/src/subscriber_node.cpp
+++ src/sus2/src/subscriber_node.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include "sus2/person_data.h"
 
-void writeMsgToLOg(const sus2::person_data::ConstPtr& persodata){
-    ROS_INFO("The message that we received was: %s", msg->data.c_str());
+void writeMsgToLOg(const sus2::person_data::ConstPtr& person_data){
+    ROS_INFO("Name: %s", person_data.name.c_str());
+    ROS_INFO("Age: %i", person_data.age);
+    ROS_INFO("Color: %s", person_data.color.c_str());
 }
 
 int main(int argc, char **argv){
